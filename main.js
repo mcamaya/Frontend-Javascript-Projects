8f46c3d0f1aa14a4b9fdd1326c9f78b8cf5cf657
Camila Amaya <camiamayar@gmail.com>
2023-05-03 18:28:01 -0500

    feat: :sparkles: Navbar desplegable mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burger = document.querySelector('.menu');
+const mobileSidebar = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burger.addEventListener('click', toggleSidebarMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
 } 
+
+function toggleSidebarMenu() {
+    mobileSidebar.classList.toggle('inactive');
+}
