f77d9246485c43604081084e0a80665617bacf25
mcamaya <camiamayar@gmail.com>
2023-05-03 11:39:57 -0500

    feat: Funcionalidad js de una tienda virtual (experimental)

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 const titulo = document.querySelector('h1');
-const numero1 = document.querySelector('#calc1');
-const numero2 = document.querySelector('#calc2');
-const boton = document.querySelector('#btnCalc');
-const resultado = document.querySelector('#result');
-const form = document.querySelector('form');
+const carrito = document.querySelector('.carritoDeCompras');
+const carrGalleta = document.querySelector('.carritoGalleta');
+const carrSoda = document.querySelector('.carritoSoda');
+const carrCafe = document.querySelector('.carritoCafe');
+const btnGalletas = document.querySelector('#btnGalletas');
+const btnSoda = document.querySelector('#btnSoda');
+const btnCafe = document.querySelector('#btnCafe');
+
+let galleta = 0;
+let soda = 0;
+let cafe = 0;
+let precioGalletas;
+let precioSoda;
+let precioCafe;
+
+btnGalletas.addEventListener('click', addGalleta);
+btnSoda.addEventListener('click', addSoda);
+btnCafe.addEventListener('click', addCafe);
 
 
-form.addEventListener('submit', onClick);
+function addGalleta (event){
+    console.log({event});
+    galleta++;
+    precioGalletas = 9.00 * galleta;
+    carrGalleta.innerHTML = `<h2>Galletas</h2> <p>Cantidad x${galleta} unidades ------ $${precioGalletas}</p>`
+    console.log(galleta);
+};
 
+function addSoda (event){
+    console.log({event});
+    soda++;
+    precioSoda = 5.00 * soda;
+    carrSoda.innerHTML = `<h2>Soda</h2> <p>Cantidad x${soda} unidades ------ $${precioSoda}</p>`
+    console.log(soda);
+};
 
-function onClick(event){
-   console.log({event});
-   event.preventDefault();
-   const suma = parseInt(numero1.value) + parseInt(numero2.value);
-   resultado.innerText = "Resultado: " + suma;
-}
+function addCafe (event){
+    console.log({event});
+    cafe++;
+    precioCafe = 7.00 * cafe;
+    carrCafe.innerHTML = `<h2>Cafe</h2> <p>Cantidad x${cafe} unidades ------ $${precioCafe}</p>`
+    console.log(cafe);
+};
