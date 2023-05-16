2fcf44d48685853e5cc3687193434e0a6507630d
mcamaya <camiamayar@gmail.com>
2023-05-16 12:23:28 -0500

    feat: :sparkles: Asincronía con generators

---
+++ src/generator/reto.js
@@ -0,0 +1,20 @@
+/* import fetch from 'node-fetch';
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi) {
+    const res = fetch(urlApi);
+    const data = res.json();
+    return data;
+}
+
+function myData() {
+    const arrayData = () => {
+        for(datos of dataArray) {
+            yield datos;
+        }
+    }
+
+    console.log();
+}
+
+myData(fetchData(`${API}/products`)); */
