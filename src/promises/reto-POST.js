f54c13199da682a1dd764e80e7fff096ac084522
mcamaya <camiamayar@gmail.com>
2023-05-16 11:11:27 -0500

    feat: :sparkles: método POST usando promesas --> Añadiendo productos a la API

--- src/promises/reto-POST.js
+++ src/promises/reto-POST.js
@@ -3,8 +3,29 @@ const API = 'https://api.escuelajs.co/api/v1';
 
 function postData(urlApi, data) {
     const response = fetch(urlApi, {
-        method : 'POST', //Método a utilizar (obtener, agregar, editar, eliminar)
+        method : 'POST', //Método a utilizar (GET, POST, PUT (actualizar), DELETE)
         mode: 'cors', //Permisos
-        credentials: 'same-origin' //Podemos omitir esta línea de código (este es el por defecto)
-    })
+        credentials: 'same-origin', //Podemos omitir esta línea de código (este es el por defecto)
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
 }
+
+const newProduct = {
+    "title": "Gorra Fashion Nike",
+    "price": 1600,
+    "description": "Una gorra muy cara :)",
+    "categoryId": 1,
+    "images": [
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/5c4ff214-5b12-44cd-88f9-62e65848cef0/gorra-de-running-perforada-dri-fit-aerobill-featherlight-h1mV7m.png"
+    ]
+};
+
+postData(`${API}/products`, newProduct)
+    .then(response => response.json())
+    .then(newProduct => console.log(newProduct))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Petición finalizada'));
