0f956d9a01c6fd8e18c29d3632b51d06074699e9
Camila Amaya <camiamayar@gmail.com>
2023-05-15 20:32:23 -0500

    perf: :zap: Nuevo script dentro de package.json para ejecutar el archivo desde la terminal con npm

--- src/callback/reto.js
+++ src/callback/reto.js
@@ -23,12 +23,12 @@ function fetchData(urlApi, callback) {
 
 fetchData(`${API}/products`, function (error1, data1){ //Traemos todos los productos de la API
     if (error1) return console.error(error1);
-    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2) { //Seleccionamos el primer producto desde data1
+    fetchData(`${API}/products/${data1[1].id}`, function (error2, data2) { //Seleccionamos el primer producto desde data1
         if (error2) return console.error(error2);
         fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3) { //Sacamos categoría del producto en data2
             if (error3) return console.error(error3);
             
-            console.log(data1[0]); //Traemos todo el primer producto
+            console.log(data1[1]); //Traemos todo el primer producto
             console.log(data2.title); //Traemos el título del producto
             console.log(data3.name); //Traemos el nombre de la categoría del producto
         })
